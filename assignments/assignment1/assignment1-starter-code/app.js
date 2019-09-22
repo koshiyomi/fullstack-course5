d8bf24bbb9eeef5658b05a61495362204a13e0ea
kevin <mashirowhite333@gmail.com>
2019-09-22 02:43:20 -0400

    9.20 STUDY

--- assignments/assignment1/assignment1-starter-code/app.js
+++ assignments/assignment1/assignment1-starter-code/app.js
@@ -4,14 +4,20 @@
   angular.module('Assignment1App', [])
   .controller('LunchCheckerController', LunchCheckerController);
 
-  function LunchCheckerController ($scope, $filter) {
+  LunchCheckerController.$inject = ['$scope'];
+
+  function LunchCheckerController ($scope) {
     $scope.isTooMuch = "Please enter data first";
 
     $scope.checkLunch = function () {
-      if ($scope.lunchList.split(",").length > 3){
-        $scope.isTooMuch = "Enjoy!";
-      }else{
-        $scope.isTooMuch = "Too much!";
+      if ($scope.lunchList.length === 0){
+        $scope.isTooMuch = "Please enter data first";
+      }else {
+        if ($scope.lunchList.split(",").length <= 3) {
+          $scope.isTooMuch = "Enjoy!";
+        } else {
+          $scope.isTooMuch = "Too much!";
+        }
       }
     };
   }
